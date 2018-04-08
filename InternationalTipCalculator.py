b5d995f78ec9af148e1a5b6271d35eb4b9b53201
Gary McDonald <mcdonagj@dukes.jmu.edu>
2018-04-08 14:23:40 -0400

    Start using Requests for web scraping.

--- InternationalTipCalculator.py
+++ InternationalTipCalculator.py
@@ -1,43 +1,52 @@
-#!/usr/bin/python
-
 ####################################################
 # Project Name: International Tip Calculator V5.0. #
 #                                                  #
 # Created By: Gary McDonald (mcdonagj)             #
-# Date: 12/13/2016.                                #
+# Date: 04/08/2018.                                #
 #                                                  #
 # Key Notes:                                       #
-# - Please use Python 2.7 to preserve the          #
+# - Please use Python 3.6 to preserve the          #
 #   intended functionality of this program.        #
 #                                                  #
-# - This program can be run by either running      #
-#    the .py file within the IDLE IDE or           #
-#     by double-clicking on the program file       #
-#      on the desktop. This opens a command        #
-#        prompt that shows the program.            #
+# - This program can be run by either running the  #
+#   .py file within IDLE or by double-clicking on  #
+#   the program file. This opens a command prompt  #
+#   that shows the output of the program.          #
+#
+# - The original version of this program was built #
+#   using Python 2.7, which included various       #
+#   libraries, such as urllib2, that controlled    #
+#   web scraping behavior. This functionality      #
+#   has been divided between multiple functions    #
+#   in the transition from Python 2.7 to           #
+#   Python 3.6. You can read more about these      #
+#   changes in the link provided below:            #
+#                                                  #
+#   https://docs.python.org/2/library/urllib2.html #
 ####################################################
 
-class InternationalTipCalculator:
 
-    import urllib2
-    import os
-    
-    print("Welcome to the International Tip Calculator!\n")    
+class InternationalTipCalculator:
+    print("Welcome to the International Tip Calculator!\n")
+
+    # r = requests.get('http://data.fixer.io/api/latest?access_key=a6cf5db13abce0db6576c936b74eeef3&format=1')
+    # r = requests.get('http://data.fixer.io/api/convert')
+    # print(r.text)
 
     print("Here are the available currencies: ")
-
-    currencies = ('1. USD - US Dollar', '2. EUR - Euro', '3. GBP - British Pound',
-                  '4. JPY - Japanese Yen', '5. CNY - Chinese Yuan')
+    currencies = (
+        '1. USD - US Dollar', '2. EUR - Euro', '3. GBP - British Pound', '4. JPY - Japanese Yen',
+        '5. CNY - Chinese Yuan')
 
     shortenedCurrencies = ('USD', 'EUR', 'GBP', 'JPY', 'CNY')
 
-    print(currencies)    
+    print(currencies)
 
-# Ask the user which currency they are using.
+    # Ask the user which currency they are using.
     validInput = False
     while not validInput:
         try:
-            currentCurrency = int(raw_input("\nWhich currency would you like to use? "))
+            currentCurrency = int(input("\nWhich currency would you like to use? "))
 
             if not (1 <= currentCurrency <= 5):
                 raise ValueError()
@@ -53,132 +62,119 @@ class InternationalTipCalculator:
             usersCurrency = currencies[currentCurrency - 1]
 
             print("Your choice is: " + usersCurrency)
-            
-# Ask the user to enter an amount:
-        mealTotal = float(raw_input("\nPlease enter the total of your meal: "))
 
-# Show out the amount of the meal:
+        # Ask the user to enter an amount:
+        mealTotal = float(input("\nPlease enter the total of your meal: "))
 
-    print "The total of your meal is: " + str(mealTotal) + shortenedCurrencies[currentCurrency - 1] + "."
+    # Show out the amount of the meal:
 
+    print("The total of your meal is: " + str(mealTotal) + shortenedCurrencies[currentCurrency - 1] + ".")
 
-# Ask the user how much they would like to tip:
-    print "\nOn average, tip percentage is between 15% to 20%."
+    # Ask the user how much they would like to tip:
+    print("\nOn average, tip percentage is between 15% to 20%.")
 
-    tipPercentage = raw_input("What percentage would you like to tip? ")
+    tipPercentage = input("What percentage would you like to tip? ")
 
-    print "You would like to tip: " + str(tipPercentage) + "%."
+    print("You would like to tip: " + str(tipPercentage) + "%.")
 
-
-# Calculate the amount of the tip:
+    # Calculate the amount of the tip:
 
     convertedTip = float(tipPercentage) / 100.00
 
-    tipAmount = mealTotal * (convertedTip)
+    tipAmount = mealTotal * convertedTip
 
-    print "\nThe amount of your tip is: " + str(tipAmount)
-
+    print("\nThe amount of your tip is: " + str(tipAmount))
 
     mealPlusTip = (mealTotal + tipAmount)
 
-    print "Your total is: " + str(mealPlusTip)
+    print("Your total is: " + str(mealPlusTip))
 
+    print("\nWould you like to convert your total to another currency? ")
 
-    print "\nWould you like to convert your total to another currency? "
-
-    userChoice = str(raw_input("Indicate your choice by typing either: (yes) or (no). "))
+    userChoice = str(input("Indicate your choice by typing either: (yes) or (no). "))
 
     if "yes" in userChoice:
         validInput = False
-        while (validInput != True):
+        while not validInput:
             try:
-                currentCurrencyB = int(
-                    raw_input("Which currency would you like to use? "))
+                currentCurrencyB = int(input("Which currency would you like to use? "))
 
                 if not (1 <= currentCurrencyB <= 5):
                     raise ValueError()
 
             except ValueError:
-                print "Invalid Option, you needed to type 1, 2, 3, 4, or 5.."
+                print("Invalid Option, you needed to type 1, 2, 3, 4, or 5..")
 
                 validInput = False
 
             else:
                 validInput = True
 
-                convertionChoice = shortenedCurrencies[currentCurrencyB - 1]
+                conversionChoice = shortenedCurrencies[currentCurrencyB - 1]
 
-                print "Your choice is: ", convertionChoice
+                print("Your choice is: ", conversionChoice)
 
+        print("Your meal, plus tip, was: " + str(mealPlusTip))
 
-        print "Your meal, plus tip, was: " + str(mealPlusTip)
+        print("You selected " + conversionChoice + " as the currency you would like to convert to.")
 
-        print "You selected " + convertionChoice + " as the currency you would like to convert to."
-        
-        #Calculate the currency rate for the desired currencies.
-        def my_rates(currentCurrency, currentCurrencyB):
+        # Calculate the currency rate for the desired currencies.
+        def my_rates(self, currentCurrencyB):
 
-            import urllib2
-            import re
-            shortenedCurrencies = ('USD', 'EUR', 'GBP', 'JPY', 'CNY')
+            # import urllib2
 
-            #Print line used to see what value is assigned to currentCurrency parameter.
-            #print (currentCurrency)
+            # shortenedCurrencies = ('USD', 'EUR', 'GBP', 'JPY', 'CNY')
 
-            #Print line used to see what value is assigned to currentCurrencyB parameter.
-            #print (currentCurrencyB)
+            # Print line used to see what value is assigned to currentCurrency parameter.
+            # print (currentCurrency)
 
-            A = shortenedCurrencies[currentCurrency - 1]
-            convertionChoice = shortenedCurrencies[currentCurrencyB - 1]
-            
-            currentConversion = A + convertionChoice + "=X"
+            # Print line used to see what value is assigned to currentCurrencyB parameter.
+            # print (currentCurrencyB)
 
-            #Print line used to see what value is assigned to currentConversion variable.
-            #print (currentConversion)
+            # A = shortenedCurrencies[self - 1]
+            # convertionChoice = shortenedCurrencies[currentCurrencyB - 1]
+
+            # currentConversion = A + convertionChoice + "=X"
+
+            # Print line used to see what value is assigned to currentConversion variable.
+            # print (currentConversion)
 
             # build the URL for the currency and fields
 
-            #curren = "USDEUR=X"  # rate from USD to EUR
-            fields = "sl1d1t1"   # query, rate, date, time
+            # curren = "USDEUR=X"  # rate from USD to EUR
+            # fields = "sl1d1t1"  # query, rate, date, time
 
-            curren = currentConversion
-            #url = "api.fixer.io/latest" + curren + "&f=" + field
-            url = "api.fixer.io/latest"
-            #url = "http://finance.yahoo.com/d/quotes.csv?s=" + curren + "&f=" + fields
-            data = urllib2.urlopen(url).read()
+            # curren = currentConversion
+            # url = "api.fixer.io/latest" + curren + "&f=" + field
+            # url = "api.fixer.io/latest"
+            # url = "http://finance.yahoo.com/d/quotes.csv?s=" + curren + "&f=" + fields
+            # data = urllib2.urlopen(url).read()
 
             # example response: "USDEUR=X",0.9415,"12/12/2016","2:56pm"
             # Print line used to test data variable output.
-            #print (data)
-                        
-            rate = float(data.split(',')[1])
+            # print (data)
+
+            # rate = float(data.split(',')[1])
 
             # Print line used to test rate variable output.
-            #print (rate)
+            # print (rate)
 
             # returns the current currency rate for the given currencies.
-            return (rate)
+            # return rate
+            return 0
 
             # Old print line used to show data and rate variables.
-            #print (data, rate)
+            # print (data, rate)
 
         currencyRate = my_rates(currentCurrency, currentCurrencyB)
-
-        print "The current rate for " + convertionChoice + " is: " + str(currencyRate)
+        print("The current rate for " + conversionChoice + " is: " + str(currencyRate))
 
         convertedTotal = mealPlusTip * currencyRate
-
-        print "Your new total is: " + str(convertedTotal) + convertionChoice ,"."
-        
-        os.system('pause')
-
+        print("Your new total is: " + str(convertedTotal) + conversionChoice, ".")
 
     elif "no" in userChoice:
-        print "\nThanks for using the International Tip Calculator!"
-
-        print "Your total is: " + str(mealPlusTip) + shortenedCurrencies[currentCurrency - 1], "."
-
-        os.system('pause')
+        print("\nThanks for using the International Tip Calculator!")
+        print("Your total is: " + str(mealPlusTip) + shortenedCurrencies[currentCurrency - 1], ".")
 
     else:
-        print "Invalid choice! Please try again!"
+        print("Invalid choice. Please try again.")
