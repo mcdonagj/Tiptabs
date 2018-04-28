9643aba3b839e209cfc21cdfd4325288ce4302d9
Gary McDonald <mcdonagj@dukes.jmu.edu>
2018-04-28 19:05:06 -0400

    Modify structure of ITC body.

--- InternationalTipCalculator.py
+++ InternationalTipCalculator.py
@@ -1,4 +1,4 @@
-# # # #
+# # # # #
 # Project Name: International Tip Calculator V5.0. #
 #                                                  #
 # Created By: Gary McDonald (mcdonagj)             #
@@ -24,10 +24,59 @@
 #   changes in the link provided below:            #
 #                                                  #
 #   https://docs.python.org/2/library/urllib2.html #
-# # # #
+# # # # #
+
+
+def my_rates(currentCurrencyB):
+
+    # import urllib2
+
+    # shortenedCurrencies = ('USD', 'EUR', 'GBP', 'JPY', 'CNY')
+
+    # Print line used to see what value is assigned to currentCurrency parameter.
+    # print (currentCurrency)
+
+    # Print line used to see what value is assigned to currentCurrencyB parameter.
+    # print (currentCurrencyB)
+
+    # A = shortenedCurrencies[self - 1]
+    # convertionChoice = shortenedCurrencies[currentCurrencyB - 1]
+
+    # currentConversion = A + convertionChoice + "=X"
+
+    # Print line used to see what value is assigned to currentConversion variable.
+    # print (currentConversion)
+
+    # build the URL for the currency and fields
+
+    # curren = "USDEUR=X"  # rate from USD to EUR
+    # fields = "sl1d1t1"  # query, rate, date, time
+
+    # curren = currentConversion
+    # url = "api.fixer.io/latest" + curren + "&f=" + field
+    # url = "api.fixer.io/latest"
+    # url = "http://finance.yahoo.com/d/quotes.csv?s=" + curren + "&f=" + fields
+    # data = urllib2.urlopen(url).read()
+
+    # example response: "USDEUR=X",0.9415,"12/12/2016","2:56pm"
+    # Print line used to test data variable output.
+    # print (data)
+
+    # rate = float(data.split(',')[1])
+
+    # Print line used to test rate variable output.
+    # print (rate)
+
+    # returns the current currency rate for the given currencies.
+    # return rate
+    return 0
+
+    # Old print line used to show data and rate variables.
+    # print (data, rate)
 
 
 class InternationalTipCalculator:
+
     print("Welcome to the International Tip Calculator!\n")
 
     # r = requests.get('http://data.fixer.io/api/latest?access_key=a6cf5db13abce0db6576c936b74eeef3&format=1')
@@ -122,52 +171,6 @@ class InternationalTipCalculator:
         print("You selected " + conversionChoice + " as the currency you would like to convert to.")
 
         # Calculate the currency rate for the desired currencies.
-        def my_rates(self, currentCurrencyB):
-
-            # import urllib2
-
-            # shortenedCurrencies = ('USD', 'EUR', 'GBP', 'JPY', 'CNY')
-
-            # Print line used to see what value is assigned to currentCurrency parameter.
-            # print (currentCurrency)
-
-            # Print line used to see what value is assigned to currentCurrencyB parameter.
-            # print (currentCurrencyB)
-
-            # A = shortenedCurrencies[self - 1]
-            # convertionChoice = shortenedCurrencies[currentCurrencyB - 1]
-
-            # currentConversion = A + convertionChoice + "=X"
-
-            # Print line used to see what value is assigned to currentConversion variable.
-            # print (currentConversion)
-
-            # build the URL for the currency and fields
-
-            # curren = "USDEUR=X"  # rate from USD to EUR
-            # fields = "sl1d1t1"  # query, rate, date, time
-
-            # curren = currentConversion
-            # url = "api.fixer.io/latest" + curren + "&f=" + field
-            # url = "api.fixer.io/latest"
-            # url = "http://finance.yahoo.com/d/quotes.csv?s=" + curren + "&f=" + fields
-            # data = urllib2.urlopen(url).read()
-
-            # example response: "USDEUR=X",0.9415,"12/12/2016","2:56pm"
-            # Print line used to test data variable output.
-            # print (data)
-
-            # rate = float(data.split(',')[1])
-
-            # Print line used to test rate variable output.
-            # print (rate)
-
-            # returns the current currency rate for the given currencies.
-            # return rate
-            return 0
-
-            # Old print line used to show data and rate variables.
-            # print (data, rate)
 
         currencyRate = my_rates(currentCurrency, currentCurrencyB)
         print("The current rate for " + conversionChoice + " is: " + str(currencyRate))
