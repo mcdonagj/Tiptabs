511ef338b061c7d5e7ee956dccb74caa3c5f52c7
Gary McDonald <mcdonagj@dukes.jmu.edu>
2018-05-15 16:39:59 -0400

    Create main.py and organize ITC to be more object-oriented.

--- InternationalTipCalculator.py
+++ InternationalTipCalculator.py
@@ -30,36 +30,10 @@
 #       retrieve currency conversion rates.        #
 ####################################################
 import sys
-
 import requests
-
-def main():
-    print("Hello")
-
-
-
-# request_rates() - Builds the current rates variable with current rate values.
-#                   Prints an error message to the terminal if the status code is not HTTP[200] (OK).
-def request_rates() -> bool:
-    current_rates = requests.get('http://data.fixer.io/api/latest?access_key=a6cf5db13abce0db6576c936b74eeef3&format=1')
-    ok_response = current_rates.status_code = 200
-    if ok_response:
-        print("Valid response from fixer.io.")
-    else:
-        print("ERROR code" + str(current_rates.status_code) + ": invalid response from fixer.io:")
-
-    return ok_response
-
-
-def get_rates(service_up: bool) -> str:
-
-    # Check to see if the service is available.
-    if not service_up:
-        print("ERROR: rate service 'fixer.io' is not available. Try again later.")
-        sys.exit()
-
-    return requests.get('http://data.fixer.io/api/latest?access_key=a6cf5db13abce0db6576c936b74eeef3&format=1').text
-
+import smtplib
+from time import gmtime, strftime
+from email.message import EmailMessage
 
 def my_rates(currentCurrencyB):
 
@@ -109,57 +83,11 @@ def my_rates(currentCurrencyB):
     # print (data, rate)
 
 
-import smtplib
-from time import gmtime, strftime
-
-class InternationalTipCalculator:
-
-    def __init__(self):
-        print("Create an International Tip Calculator")
-
     print("Welcome to the International Tip Calculator!\n")
 
-    exec_rates = request_rates()
-
-    # TODO: Implement functionality that sends an email notification to mcdonagj@dukes.jmu.edu
-    # when the request for rate information fails.
-    # Library: smtplib
-    # https://www.tutorialspoint.com/python/python_sending_email.htm
-    # Used for testing email messaging.
-
-    if not exec_rates:
-        print("Enter error check.")
-        from email.message import EmailMessage
-        print("Import email package.")
-        send_msg = EmailMessage()
-        print("Create EmailMessage object.")
-        send_msg['Subject'] = '[ERROR] International Tip Calculator - ' + strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        send_msg['To'] = 'mcdonagj@dukes.jmu.edu'
-        send_msg.preamble = "There was a problem with retrieving rates in the International Tip Calculator:" + "\n" + "\tDate: " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + "\n"
-
-        print("Message built.")
-
-        gmail_user = 'sendpyerr@gmail.com'
-        gmail_pwd = 'pythonerr'
-        FROM = 'sendpyerr@gmail.com'
-        TO = 'mcdonagj@dukes.jmu.edu'
-
-        smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
-        smtpserver.ehlo()
-        smtpserver.starttls()
-        smtpserver.ehlo()
-        smtpserver.login(gmail_user, gmail_pwd)
-
-        # Send an email to the desired location.
-        message_as_string = send_msg.preamble
-
-        smtpserver.sendmail(FROM, TO, send_msg.as_string())
-
-        sys.exit()
-
-    rates = get_rates(exec_rates)
-
     print("Here are the available currencies: ")
+
+    currencies = ['USD - US Dollar', 'EUR - Euro', 'GBP - British Pound', 'JPY - Japanese Yen', 'CNY - Chinese Yuan']
     currencies = (
         '1. USD - US Dollar', '2. EUR - Euro', '3. GBP - British Pound', '4. JPY - Japanese Yen',
         '5. CNY - Chinese Yuan')
@@ -257,5 +185,79 @@ class InternationalTipCalculator:
         print("Invalid choice. Please try again.")
 
 
-if __name__ == '__name__':
-    main()
+class InternationalTipCalculator(object):
+
+    base = ""
+    currencies = []
+    rates = ""
+
+    def __init__(self, desired_base: str):
+        if len(desired_base) > 0:
+            self.base = desired_base
+        else:
+            self.base = 'EUR'
+
+    # TODO: Complete vigorous error checking on parameters.
+    def set_base(self, desired_base: str):
+        self.base = desired_base
+        return
+
+    def get_base(self):
+        return self.base
+
+    def make_rates_calc(self, desired_rates):
+        self.rates = desired_rates
+
+    def make_currency_calc(self, desired_currencies):
+        self.currencies = desired_currencies
+
+    def make_custom_calc(self, desired_currencies, desired_rates):
+        self.currencies = desired_currencies
+        self.rates = desired_rates
+
+    # request_rates() - Builds the current rates variable with current rate values.
+    #                   Prints an error message to the terminal if the status code is not HTTP[200] (OK).
+    def request_rates(self) -> bool:
+        current_rates = requests.get(
+            'http://data.fixer.io/api/latest?access_key=a6cf5db13abce0db6576c936b74eeef3&format=1')
+        ok_response = current_rates.status_code == 200
+        if ok_response:
+            print("Valid response from fixer.io.")
+        else:
+            print("ERROR code" + str(current_rates.status_code) + ": invalid response from fixer.io:")
+
+        return ok_response
+
+    def get_rates(self, service_up: bool) -> str:
+
+        # Check to see if the service is available.
+        if not service_up:
+            print("ERROR: rate service 'fixer.io' is not available. Try again later.")
+            sys.exit()
+
+        return requests.get('http://data.fixer.io/api/latest?access_key=a6cf5db13abce0db6576c936b74eeef3&format=1').text
+
+    def send_error_message(self):
+        send_msg = EmailMessage()
+
+        send_msg['Subject'] = '[ERROR] International Tip Calculator - ' + strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        send_msg['To'] = 'mcdonagj@dukes.jmu.edu'
+        send_msg.preamble = "There was a problem with retrieving rates in the International Tip Calculator:" + "\n" + "\tDate: " + strftime(
+            "%Y-%m-%d %H:%M:%S", gmtime()) + "\n"
+
+        gmail_user = 'sendpyerr@gmail.com'
+        gmail_pwd = 'pythonerr'
+        FROM = 'sendpyerr@gmail.com'
+        TO = 'mcdonagj@dukes.jmu.edu'
+
+        smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+        smtpserver.ehlo()
+        smtpserver.starttls()
+        smtpserver.ehlo()
+        smtpserver.login(gmail_user, gmail_pwd)
+
+        # Send an email to the desired location.
+        message_as_string = send_msg.preamble
+
+        smtpserver.sendmail(FROM, TO, send_msg.as_string())
+
---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+
+from InternationalTipCalculator import InternationalTipCalculator
+
+
+def main():
+
+    itc: InternationalTipCalculator = InternationalTipCalculator("EUR")
+
+    exec_rates = itc.request_rates()
+
+    # TODO: Implement functionality that sends an email notification to mcdonagj@dukes.jmu.edu
+    # when the request for rate information fails.
+    # Library: smtplib
+    # https://www.tutorialspoint.com/python/python_sending_email.htm
+    # Used for testing email messaging.
+
+    if not exec_rates:
+        itc.send_error_message()
+        sys.exit()
+
+    rates = itc.get_rates(exec_rates)
+    print(rates)
+
+
+if __name__ == '__main__':
+    main()
+
+
