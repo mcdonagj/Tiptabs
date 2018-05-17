b79eaf12cce1f6f4056f1e93c06fd291d4ef935a
Gary McDonald <mcdonagj@dukes.jmu.edu>
2018-05-17 18:23:13 -0400

    Refactor functionality in main.py.

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 from InternationalTipCalculator import *
-from itc_gui import *
+from DictionaryBuilder import *
+from UserInterface import *
 
 
 def main():
 
-    itc: InternationalTipCalculator = InternationalTipCalculator("EUR")
-
-    exec_rates = itc.request_rates()
+    dictionary: DictionaryBuilder = DictionaryBuilder()
 
     # TODO: Implement functionality that sends an email notification to mcdonagj@dukes.jmu.edu
     # when the request for rate information fails.
     # Library: smtplib
     # https://www.tutorialspoint.com/python/python_sending_email.htm
     # Used for testing email messaging.
+    exec_rates = dictionary.request_rates()
 
     if not exec_rates:
-        itc.send_error_message()
+        dictionary.send_error_message()
         sys.exit()
 
-    rates = itc.get_rates(exec_rates)
-    print(rates)
+    # Populates the dictionary with available currencies.
+    rates = dictionary.get_rates(exec_rates)
 
-    #TODO: Implement a graphical user internface for the International Tip Calculator.
-    ui: itcGUI = itcGUI("International Tip Calculator")
+    # TODO: Implement a graphical user interface for the International Tip Calculator.
+    # ui: itcGUI = itcGUI("International Tip Calculator")
 
+    # TODO: Integrate user input from GUI to allow for entry of a given rate.
+    itc: InternationalTipCalculator = InternationalTipCalculator("EUR", dictionary)
+
+    #print(len(dictionary.currencies))
+
+    #print(dictionary.check_available_bases('ZWL'))
+
+    #print(rates)
 
 if __name__ == '__main__':
     main()
