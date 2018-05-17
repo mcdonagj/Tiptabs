5e9452fc52310ebddf138347af6c89e76f16e2a9
Gary McDonald <mcdonagj@dukes.jmu.edu>
2018-05-17 18:22:34 -0400

    Rename itc_gui to UserInterface.py. Class functionality under construction.

---
+++ UserInterface.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+
+class UserInterface:
+
+    def __init__(self, title):
+        print("GUI under construction.")
+
+    #top = Tk()
+
+    #def __init__(self, title):
+        #self.top.title(title)
+        #self.top.resizable(False, False)
+        #self.top.geometry('400x400')
+        #self.top.mainloop()
+
+
+
+
