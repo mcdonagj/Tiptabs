e145b0452432cd886c9d13ca7f4563d03d13c140
Gary McDonald <mcdonagj@dukes.jmu.edu>
2018-05-18 19:51:30 -0400

    Modify object calls to better illustrate class structure.

--- InternationalTipCalculator.py
+++ InternationalTipCalculator.py
@@ -1,24 +1,25 @@
-
 from DictionaryBuilder import *
 
 
 class InternationalTipCalculator:
-
     base = ""
+    dictionary_builder = None
 
-    def __init__(self, desired_base: str, dict: DictionaryBuilder):
+    def __init__(self, desired_base: str, desired_dictionary: DictionaryBuilder):
         """
         __init__(self, str, DictionaryBuilder): Constructor for creating an InternationalTipCalculator.
         :param desired_base: Desired currency base.
-        :param dict: Constructed dictionary with available bases and rates for a given day.
+        :param desired_dictionary: Constructed dictionary with available bases and rates for a given day.
         """
 
         chk_base_size = len(desired_base) > 0
 
+        self.dictionary_builder = desired_dictionary
+
         if not chk_base_size:
             self.base = 'EUR'
         else:
-            base_available = dict.check_available_bases(desired_base)
+            base_available = desired_dictionary.check_available_bases(desired_base)
             if base_available:
                 self.base = desired_base
             else:
@@ -28,7 +29,7 @@ class InternationalTipCalculator:
         return self.base
 
     def set_base(self, desired_base: str) -> bool:
-        base_available = dict.check_available_bases(desired_base)
+        base_available = self.dictionary_builder.check_available_bases(desired_base)
 
         if base_available:
             self.base = desired_base
