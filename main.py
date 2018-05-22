d16f45a6efd09d5d5f723ecec2791add16d8ab77
Gary McDonald <mcdonagj@dukes.jmu.edu>
2018-05-22 14:10:42 -0400

    Add basic implementation for creating a web application using Flask.
    Revise documentation in main.py.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from InternationalTipCalculator import *
 from DictionaryBuilder import *
 from UserInterface import *
+from flask import Flask
 
 
 def main():
@@ -10,31 +11,45 @@ def main():
 
     dictionary_builder: DictionaryBuilder = DictionaryBuilder()
 
-    # TODO: Implement functionality that sends an email notification to mcdonagj@dukes.jmu.edu
-    # when the request for rate information fails.
-    # Library: smtplib
-    # https://www.tutorialspoint.com/python/python_sending_email.htm
-    # Used for testing email messaging.
+    # Retrieve the rates for a given base currency.
     exec_rates = dictionary_builder.request_rates()
 
+    # If the retrieval of rates does not succeed, send an email with details of the event.
     if not exec_rates:
         dictionary_builder.send_error_message()
         sys.exit()
 
-    # Populates the dictionary with available currencies.
+    # Populate the dictionary with available currencies.
     rates = dictionary_builder.get_rates(exec_rates)
 
     # TODO: Integrate user input from GUI to allow for entry of a given rate.
     itc: InternationalTipCalculator = InternationalTipCalculator("EUR", dictionary_builder)
 
     # TODO: Implement a graphical user interface for the International Tip Calculator.
-    ui: UserInterface = UserInterface("International Tip Calculator", itc)
+    #ui: UserInterface = UserInterface("International Tip Calculator", itc)
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return "Welcome to the International Tip Calculator."
+
+    @app.route('/fixer_status')
+    def fixer_status():
+        result = "Fixer.io is not available."
+        if exec_rates:
+            result = "Fixer.io is available for use."
+
+        return result
+
+    app.run(debug=True)
 
     #print(len(dictionary_builder.currencies))
 
     #print(dictionary_builder.check_available_bases('ZWL'))
 
     #print(rates)
+
 
 if __name__ == '__main__':
     main()
