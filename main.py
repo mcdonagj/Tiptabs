94ee3968858f24176e7356dfa4e3459629e895a3
Gary McDonald <mcdonagj@dukes.jmu.edu>
2018-07-09 21:30:27 -0400

    Update host and port to be platform independent.
    Signed-off-by: Gary McDonald <mcdonagj@dukes.jmu.edu>

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def main():
             result = "Fixer.io is available for use."
         return render_template("fixer_status.html", result=result)
 
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
 
 
 if __name__ == '__main__':
