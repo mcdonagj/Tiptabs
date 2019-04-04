a7c64a992524065d23fa229626d2d580dfee79ea
Gary McDonald <mcdonagj@dukes.jmu.edu>
2019-04-03 21:58:03 -0400

    Making it better -- change main add get call.

--- Tiptabs/main.py
+++ Tiptabs/main.py
@@ -147,6 +147,9 @@ from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def get_home():
+    return render_template("app.html", rates={'ABC'})
 
 if __name__ == '__main__':
     app.run()
