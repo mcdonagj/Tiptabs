f8428dddfbc8ca5725321f013dea7905411e2149
Gary McDonald <mcdonagj@dukes.jmu.edu>
2019-04-03 21:30:52 -0400

    Making it better -- Try stuff.

--- Tiptabs/main.py
+++ Tiptabs/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
 import os
 import sys
@@ -16,134 +16,134 @@ from Tiptabs.DictionaryBuilder import *
 from flask import Flask, render_template, request
 
 def main():
-    """
-    main() - Main class for Tiptabs. The Flask web application is created in this file. 
-    Additionally, this file handles the creation of the dictionary of currencies.
-    """
+#     """
+#     main() - Main class for Tiptabs. The Flask web application is created in this file. 
+#     Additionally, this file handles the creation of the dictionary of currencies.
+#     """
 
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger(__name__)    
+#     logging.basicConfig(level=logging.DEBUG)
+#     logger = logging.getLogger(__name__)    
     
-    env_path = str(Path(dirname(dirname(abspath(__file__)))) / '.env')
-    logger.debug("Loading .env file from: {!s}".format(env_path))    
-    load_dotenv(dotenv_path=env_path)
+#     env_path = str(Path(dirname(dirname(abspath(__file__)))) / '.env')
+#     logger.debug("Loading .env file from: {!s}".format(env_path))    
+#     load_dotenv(dotenv_path=env_path)
 
-    APP_PORT = int(os.getenv("APP_PORT"))
-    logger.debug("Chosen port for application: {!s}".format(str(APP_PORT)))
+#     APP_PORT = int(os.getenv("APP_PORT"))
+#     logger.debug("Chosen port for application: {!s}".format(str(APP_PORT)))
 
-    APP_ADDRESS = str(os.getenv("APP_HOST"))
-    logger.debug("Chosen address for application: {!s}".format(APP_ADDRESS))
+#     APP_ADDRESS = str(os.getenv("APP_HOST"))
+#     logger.debug("Chosen address for application: {!s}".format(APP_ADDRESS))
 
-    phone_verifier = PhoneVerifier(str(os.getenv("SMS_FORMAT")), str(os.getenv("SMS_COUNTRY_CODE")), str(os.getenv("SMS_URL")), str(os.getenv("SMS_KEY")))
+#     phone_verifier = PhoneVerifier(str(os.getenv("SMS_FORMAT")), str(os.getenv("SMS_COUNTRY_CODE")), str(os.getenv("SMS_URL")), str(os.getenv("SMS_KEY")))
 
-    logger.debug("Rates Service being requested from: {!s}".format(str(os.getenv("RATES_URL"))))
+#     logger.debug("Rates Service being requested from: {!s}".format(str(os.getenv("RATES_URL"))))
 
-    dictionary_builder = DictionaryBuilder()
-    logger.debug("DictionaryBuilder created.")
+#     dictionary_builder = DictionaryBuilder()
+#     logger.debug("DictionaryBuilder created.")
 
-    logger.info("Check availability of Rates API. ({!s})".format(str(os.getenv("RATES_URL"))))
-    exec_rates = dictionary_builder.request_rates(str(os.getenv("RATES_URL")), str(os.getenv("RATES_KEY")), str(os.getenv("RATES_FORMAT")))
+#     logger.info("Check availability of Rates API. ({!s})".format(str(os.getenv("RATES_URL"))))
+#     exec_rates = dictionary_builder.request_rates(str(os.getenv("RATES_URL")), str(os.getenv("RATES_KEY")), str(os.getenv("RATES_FORMAT")))
 
-    if not exec_rates[0]:
-        logger.info(str(exec_rates[1]))
-        FROM_ADDRESS = str(os.getenv("FROM_ADDRESS"))
-        TO_ADDRESS = str(os.getenv("TO_ADDRESS"))
-        GMAIL_PW = str(os.getenv("GMAIL_PW"))
-        dictionary_builder.send_error_message(FROM_ADDRESS, TO_ADDRESS, GMAIL_PW)
-        sys.exit()
+#     if not exec_rates[0]:
+#         logger.info(str(exec_rates[1]))
+#         FROM_ADDRESS = str(os.getenv("FROM_ADDRESS"))
+#         TO_ADDRESS = str(os.getenv("TO_ADDRESS"))
+#         GMAIL_PW = str(os.getenv("GMAIL_PW"))
+#         dictionary_builder.send_error_message(FROM_ADDRESS, TO_ADDRESS, GMAIL_PW)
+#         sys.exit()
 
-    populate_dictionary_result = dictionary_builder.get_rates(
-        exec_rates[0], exec_rates[1])
+#     populate_dictionary_result = dictionary_builder.get_rates(
+#         exec_rates[0], exec_rates[1])
 
-    tiptabs_core = Tiptabs(str(os.getenv("STARTING_RATE")), dictionary_builder)
-    logger.info("Initialize Tiptabs core with base rate: {!s} ...".format(str(os.getenv("STARTING_RATE"))))
+#     tiptabs_core = Tiptabs(str(os.getenv("STARTING_RATE")), dictionary_builder)
+#     logger.info("Initialize Tiptabs core with base rate: {!s} ...".format(str(os.getenv("STARTING_RATE"))))
     
-    rates = list(dictionary_builder.currencies.keys())
-    logger.info("-- {!s} conversion rates succesfully recieved!".format(len(rates)))
+#     rates = list(dictionary_builder.currencies.keys())
+#     logger.info("-- {!s} conversion rates succesfully recieved!".format(len(rates)))
 
-    rates.sort()
-    logger.info("-- Sorting {!s} rates in alphanumeric order ...".format(len(rates)))
+#     rates.sort()
+#     logger.info("-- Sorting {!s} rates in alphanumeric order ...".format(len(rates)))
 
-    logger.info(" Initializing Flask application ...")
+#     logger.info(" Initializing Flask application ...")
     app = Flask(__name__)
 
-    @app.route('/', methods=['GET'])
-    def get_home():
-        logger.debug(" -- Contents of Rates: {!s}".format(str(rates)))
-        return render_template("app.html", rates=rates)
+#     @app.route('/', methods=['GET'])
+#     def get_home():
+#         logger.debug(" -- Contents of Rates: {!s}".format(str(rates)))
+#         return render_template("app.html", rates=rates)
 
-    @app.route('/', methods=['POST'])
-    def post_home():
+#     @app.route('/', methods=['POST'])
+#     def post_home():
 
-        if request.method == 'POST':
+#         if request.method == 'POST':
 
-            post_form_resp = [False, "ERROR: Request form was invalid/empty."]
+#             post_form_resp = [False, "ERROR: Request form was invalid/empty."]
 
-            if request.form:
+#             if request.form:
 
-                total_base_currency = str(request.form['base_currency'])
+#                 total_base_currency = str(request.form['base_currency'])
 
-                logger.debug(" -- Chosen Base Currency: {!s}".format(str(total_base_currency)))
+#                 logger.debug(" -- Chosen Base Currency: {!s}".format(str(total_base_currency)))
 
-                check_avail_base = dictionary_builder.check_available_bases(
-                    total_base_currency)
+#                 check_avail_base = dictionary_builder.check_available_bases(
+#                     total_base_currency)
 
-                logger.debug(" -- Is the chosen base '{}' available? : {!s}.".format(str(total_base_currency), str(check_avail_base)))
+#                 logger.debug(" -- Is the chosen base '{}' available? : {!s}.".format(str(total_base_currency), str(check_avail_base)))
 
-                if not check_avail_base:
-                    base_not_avail_resp = 'ERROR: Chosen base "{!s}" is not available.'.format(
-                        total_base_currency)
+#                 if not check_avail_base:
+#                     base_not_avail_resp = 'ERROR: Chosen base "{!s}" is not available.'.format(
+#                         total_base_currency)
 
-                    post_form_resp = [False, base_not_avail_resp]
+#                     post_form_resp = [False, base_not_avail_resp]
 
-                    # Return the response to display on app.html
-                    # return post_form_resp
+#                     # Return the response to display on app.html
+#                     # return post_form_resp
 
-                total_bill_amount = str(request.form['bill_amount'])
+#                 total_bill_amount = str(request.form['bill_amount'])
 
-                total_tip_percentage = str(request.form['tip_percentage'])
+#                 total_tip_percentage = str(request.form['tip_percentage'])
 
-                total_desr_currency = str(request.form['converted_currency'])
+#                 total_desr_currency = str(request.form['converted_currency'])
 
-                # Set the internal base to desired rate.
-                # set_base_result = tiptabs_core.set_base(total_base_currency)
+#                 # Set the internal base to desired rate.
+#                 # set_base_result = tiptabs_core.set_base(total_base_currency)
 
-                check_desr_currency = dictionary_builder.check_available_bases(
-                    total_desr_currency)
+#                 check_desr_currency = dictionary_builder.check_available_bases(
+#                     total_desr_currency)
 
-                logger.debug(" -- Is the chosen conversion currency '{!s}' available? : {!s}.".format(str(total_desr_currency), str(check_desr_currency)))
+#                 logger.debug(" -- Is the chosen conversion currency '{!s}' available? : {!s}.".format(str(total_desr_currency), str(check_desr_currency)))
 
-                total = tiptabs_core.calculate_total(
-                    total_bill_amount, total_tip_percentage, total_desr_currency)
+#                 total = tiptabs_core.calculate_total(
+#                     total_bill_amount, total_tip_percentage, total_desr_currency)
 
-                post_form_resp = total
+#                 post_form_resp = total
 
-            return render_template("result.html", resp=post_form_resp[0], result=post_form_resp[1])
+#             return render_template("result.html", resp=post_form_resp[0], result=post_form_resp[1])
 
-    @app.route('/result', methods=['POST'])
-    def send_sms(desired_number):        
-        valid_number = phone_verifier.verifyPhone(desired_number)        
-        if valid_number:
-            phone_verifier.send_sms_to_number(desired_number)
+#     @app.route('/result', methods=['POST'])
+#     def send_sms(desired_number):        
+#         valid_number = phone_verifier.verifyPhone(desired_number)        
+#         if valid_number:
+#             phone_verifier.send_sms_to_number(desired_number)
 
-    @app.route('/fixer_status', methods=['GET'])
-    def get_fixer_status():
+#     @app.route('/fixer_status', methods=['GET'])
+#     def get_fixer_status():
 
-        fxr_resp = [False, "Fixer.io is not available."]
+#         fxr_resp = [False, "Fixer.io is not available."]
 
-        if request.method == 'GET':
-            if exec_rates:
-                fxr_resp = [exec_rates, "Fixer.io is available for use."]
+#         if request.method == 'GET':
+#             if exec_rates:
+#                 fxr_resp = [exec_rates, "Fixer.io is available for use."]
 
-        return render_template("fixer_status.html", resp=fxr_resp[0], result=fxr_resp[1])
+#         return render_template("fixer_status.html", resp=fxr_resp[0], result=fxr_resp[1])
 
-    @app.route('/feedback', methods=['GET', 'POST'])
-    def send_feedback():
-        return render_template("feedback.html")
+#     @app.route('/feedback', methods=['GET', 'POST'])
+#     def send_feedback():
+#         return render_template("feedback.html")
 
-    @app.errorhandler(404)
-    def no_page_found(e):
-        return render_template('error_404.html')
+#     @app.errorhandler(404)
+#     def no_page_found(e):
+#         return render_template('error_404.html')
 
 
     port = int(os.environ.get('PORT', 5000))
--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
-from setuptools import setup
+# from setuptools import setup
 
-setup(
-    name="tiptabs",
-    version="1.0.0",
-    packages=["Tiptabs"],
-    install_requires=[
-        "Flask==0.12.3",
-        "Flask-API==0.7.1",
-        "Jinja2==2.9.6",
-        "requests==2.20.0",
-        "MySQL-connector-python==8.0.12",
-        "python-dotenv==0.10.1",
-        "boto3==1.9.119",
-        "gunicorn==19.9.0"
-    ],
-    url="https://github.com/mcdonagj/Tiptabs",
-    license="MIT",
-    author="Gary McDonald",
-    entry_points={
-        'console_scripts': ['tiptabs = Tiptabs.main:main'],
-    },
-)
+# setup(
+#     name="tiptabs",
+#     version="1.0.0",
+#     packages=["Tiptabs"],
+#     install_requires=[
+#         "Flask==0.12.3",
+#         "Flask-API==0.7.1",
+#         "Jinja2==2.9.6",
+#         "requests==2.20.0",
+#         "MySQL-connector-python==8.0.12",
+#         "python-dotenv==0.10.1",
+#         "boto3==1.9.119",
+#         "gunicorn==19.9.0"
+#     ],
+#     url="https://github.com/mcdonagj/Tiptabs",
+#     license="MIT",
+#     author="Gary McDonald",
+#     entry_points={
+#         'console_scripts': ['tiptabs = Tiptabs.main:main'],
+#     },
+# )
